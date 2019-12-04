32a5277ae1e234190b5c15a483b4cb3360bea89d
Michael Bramblett <michaelbramblett0@gmail.com>
2019-12-04 00:31:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
   puts "The line is currently empty."
 else 
-  katz_deli.each do |line|
-  puts "The line is currently: "
+  result = "The line is currently: "
+  katz_deli.each_with_index(1) do |line, i|
+  result << "#{i}. #{line}" 
 end
 end
   end
