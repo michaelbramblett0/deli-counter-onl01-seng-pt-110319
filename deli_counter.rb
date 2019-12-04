76e1cfe26501a5badd972bc0658c3aca30823882
Michael Bramblett <michaelbramblett0@gmail.com>
2019-12-04 00:11:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,5 @@ def line(katz_deli)
 
   def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. "
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
